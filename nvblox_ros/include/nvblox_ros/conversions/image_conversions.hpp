088f6fa884562a59f7b1c2c0b59778fb5640115b
Yang Zhou <yangzhou.info@gmail.com>
2025-03-13 09:24:52 -0400

    deprecate nitros from image_conversions

--- nvblox_ros/include/nvblox_ros/conversions/image_conversions.hpp
+++ nvblox_ros/include/nvblox_ros/conversions/image_conversions.hpp
@@ -29,7 +29,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-#include "nvblox_ros/nitros_types.hpp"
+// #include "nvblox_ros/nitros_types.hpp"
 #include "nvblox_ros/conversions/image_conversions_thrust.hpp"
 
 namespace nvblox
@@ -98,10 +98,10 @@ void imageMessageFromColorImage(
 /// @param logger       ROS2 logger
 /// @param cuda_stream  Cuda stream used for copying data
 /// @return True on success, False on failure.
-bool depthImageFromNitrosViewAsync(
-  const NitrosView & view,
-  DepthImage * depth_image, rclcpp::Logger logger,
-  const CudaStream & cuda_stream);
+// bool depthImageFromNitrosViewAsync(
+//   const NitrosView & view,
+//   DepthImage * depth_image, rclcpp::Logger logger,
+//   const CudaStream & cuda_stream);
 
 /// Convert Nitros image message to color frame object
 ///
@@ -112,14 +112,14 @@ bool depthImageFromNitrosViewAsync(
 /// @param logger       ROS2 logger
 /// @param cuda_stream  Cuda stream used for copying data
 /// @return True on success, False on failure.
-bool colorImageFromNitrosViewAsync(
-  const NitrosView & view,
-  ColorImage * color_image,
-  rclcpp::Logger logger, const CudaStream & cuda_stream);
+// bool colorImageFromNitrosViewAsync(
+//   const NitrosView & view,
+//   ColorImage * color_image,
+//   rclcpp::Logger logger, const CudaStream & cuda_stream);
 
-bool monoImageFromNitrosViewAsync(
-  const NitrosView & view, MonoImage * mono_image,
-  rclcpp::Logger logger, const CudaStream & cuda_stream);
+// bool monoImageFromNitrosViewAsync(
+//   const NitrosView & view, MonoImage * mono_image,
+//   rclcpp::Logger logger, const CudaStream & cuda_stream);
 
 }  // namespace conversions
 }  // namespace nvblox
